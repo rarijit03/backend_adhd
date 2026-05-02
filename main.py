d5750d74e98abc354fa04854df8685b346ea5500
rarijit03 <166358892+rarijit03@users.noreply.github.com>
2026-05-03 02:13:58 +0530

    Modify CORS settings to allow all origins
    Updated CORS middleware to allow all origins.

--- main.py
+++ main.py
@@ -47,8 +47,9 @@ app = FastAPI(title="NeuraScan API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://frontend-adhd-rarijit03s-projects.vercel.app",
+        # "http://localhost:3000",
+        # "https://frontend-adhd-rarijit03s-projects.vercel.app",
+      "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
