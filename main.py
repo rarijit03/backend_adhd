c3dac426990940d8c5efee076d4ee3d7045ca9df
rarijit03 <166358892+rarijit03@users.noreply.github.com>
2026-05-03 03:47:29 +0530

    Refine CORS middleware configuration
    Updated CORS middleware to specify allowed origins for local and production environments.

--- main.py
+++ main.py
@@ -44,24 +44,23 @@ GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
 # ── App ───────────────────────────────────────────────────────
 app = FastAPI(title="NeuraScan API", version="2.0.0")
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         # "http://localhost:3000",
-#         # "https://frontend-adhd-rarijit03s-projects.vercel.app",
-#       "*"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://frontend-adhd-git-main-rarijit03s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # ── Supabase ──────────────────────────────────────────────────
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
