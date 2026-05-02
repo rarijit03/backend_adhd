351bf56700423ff3c1806d5444ff1605ff9db695
rarijit03 <166358892+rarijit03@users.noreply.github.com>
2026-05-03 02:12:35 +0530

    Update CORS allowed origins for the app

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://frontend-adhd.vercel.app",
+        "https://frontend-adhd-rarijit03s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
